946607e34ca132b7cc67b9b67da30311109fb462
AsifKhan-2406 <pattanasifkhan151@gmail.com>
2024-04-07 10:55:21 +0530

    Updated the changes

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ document.addEventListener('DOMContentLoaded', () => {
   
       try 
       {
-        const response = await fetch('http://127.0.0.1:5000/signin', {
+        const response = await fetch('https://backend-py-2.onrender.com/signin', {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json'
