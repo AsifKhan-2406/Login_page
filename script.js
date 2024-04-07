661a686e69b0915ef60c66516394032a6e20e981
AsifKhan-2406 <146152698+AsifKhan-2406@users.noreply.github.com>
2024-04-07 10:10:04 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const loginForm = document.getElementById('loginForm');
+  
+    loginForm.addEventListener('submit', async (event) => {
+      event.preventDefault();
+  
+      const username = document.getElementById('usernameInput').value;
+      const password = document.getElementById('passwordInput').value;
+  
+      try 
+      {
+        const response = await fetch('http://127.0.0.1:5000/signin', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({ username, password })
+        });
+  
+        const data = await response.json();
+  
+        if (response.ok) {
+          // Login successful
+          alert("Login successfull !!");// Display success message
+          // You can redirect to another page or perform other actions here
+        } else {
+          // Login failed
+          alert(data.message); // Display error message
+        }
+      } 
+      catch (error) 
+      {
+        console.error('Login failed:', error);
+        alert('Login failed. Please try again.');
+      }
+    });
+  });
+  
